ed2c26507e762cc28f70c17abe0d92bc33ff50f4
Maninder Singh <mani_lion@hotmail.com>
2019-01-31 00:18:54 -0500

    Location Info

---
+++ LocationTest/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  LocationTest
+//
+//  Created by singh on 2018-02-10.
+//  Copyright © 2018 singh. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation: CLLocation = locations[locations.count - 1]
+        latitudeLabel.text = String(format: "%.6f", lastLocation.coordinate.latitude)
+        longitudeLabel.text = String(format: "%.6f", lastLocation.coordinate.longitude)
+        
+    }
+
+
+}
+
